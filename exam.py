47eb708e1d9ac1c15da20d476196a9c476e9c748
loblab <11693722+loblab@users.noreply.github.com>
2021-09-04 14:19:36 +0800

    Count and stat; rename file

--- exam.py
+++ exam.py
@@ -1,9 +1,11 @@
+#!/usr/bin/python3
+
 from question import *
 import time
 
-class Examiner:
+class Exam:
 
-    def __init__(self):
+    def __init__(self, total=10):
         templates = [
             "100 +- 100",
             "100 * 100",
@@ -11,8 +13,11 @@ class Examiner:
             "1000 +- 1000"
         ]
         self.qbank = Question(templates)
+        self.total = total
 
     def round(self):
+        self.index += 1
+        print("     No.: %d/%d" % (self.index, self.total))
         question = self.qbank.question()
         print("Question: " + question)
         anwser0 = str(int(self.qbank.anwser(question)))
@@ -22,6 +27,7 @@ class Examiner:
             anwser = input("  Anwser: ").strip().lower()
             t2 = time.time()
             dur = t2 - t1
+            self.dur += dur
             if anwser == "bye" or anwser == "quit":
                 return False
             if anwser == "next" or anwser == "skip":
@@ -29,22 +35,36 @@ class Examiner:
             elif anwser == anwser0:
                 print(" CORRECT! %.1fs" % dur)
                 err = 0
+                self.correct += 1
                 return True
             else:
                 err = 1
+                self.wrong += 1
                 print("   WRONG! %.1fs" % dur)
             retry += 1
         return True
 
-    def run(self):
-        while self.round():
-            print("")
+    def report(self):
+        print("")
+        done = self.correct + self.wrong
+        print("   Cost: %5.1fs, Average: %5.1fs" % (self.dur, self.dur / done))
+        print("Correct: %3d,      Wrong: %3d" % (self.correct, self.wrong))
+        print("  Score: %3d" % (100.0 * self.correct / done))
         print("")
         print("Bye! See you next time.")
         print("")
 
+    def run(self):
+        self.index = 0
+        self.correct = 0
+        self.wrong = 0
+        self.dur = 0
+        while self.round():
+            print("")
+        self.report()
+
 if __name__ == "__main__":
-    ex = Examiner()
-    ex.run()
+    exam = Exam()
+    exam.run()
 
 
